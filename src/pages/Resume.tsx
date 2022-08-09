4af9dccf39a7d107144789108177803bbbce9885
yefrey espinoza <yeffreyespinoza200@gmail.com>
2022-08-09 03:04:09 -0500

    upload pdf cv to github

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 const Resume = () => {
-  return <div>this is a resume</div>;
+  const [width, setWidth] = useState(1200);
+  const resumeLink =
+    "https://raw.githubusercontent.com/emily883/portfolio_Emily_/main/src/Assets/EmilyCruzGutierrezResume.pdf";
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+  return (
+    <div className="main scroll-y">
+      <div className="main-contet">
+        <div className="cv-button-container">
+          <button>Download Cv</button>
+        </div>
+        <div className="container-for-pdf">
+          <Document file={resumeLink} className="pdf-cv">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </div>
+        <div className="cv-button-container">
+          <button>Download Cv</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
