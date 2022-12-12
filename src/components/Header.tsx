3179afb400ecfabac1ace31a8ffaf4196885b410
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-11 22:58:07 -0500

    update the header and nav (refactoring code)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,68 +1,16 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./NavBar";
-import "./../assets/Header.css";
-import { CgFileDocument } from "react-icons/cg";
-import { FaTimes } from "react-icons/fa";
-import { CgMenu } from "react-icons/cg";
-import {
-  AiOutlineFundProjectionScreen,
-  AiOutlineHome,
-  AiOutlineUser,
-} from "react-icons/ai";
-const Header = () => {
-  const [isOpenNavbar, setIsOpenNavbar] = useState(false);
-  const changeNavbarState = () => setIsOpenNavbar(!isOpenNavbar);
-  return (
-    <>
-      <div className="header-container">
-        <header id="header">
-          <div className="navbar-icon" onClick={changeNavbarState}>
-            {isOpenNavbar ? <FaTimes /> : <CgMenu />}
 
-            {/* <FaTimes /> */}
-          </div>
-          <Link id="logo" to="/">
-            <span>Y.E.</span>
-          </Link>
-          <div className="navbar-container">
-            <Navbar isOpen={isOpenNavbar} onClickLeft={changeNavbarState} />
-          </div>
-          <div className="navbar-transparent"></div>
-          <div id="cv-link">
-            <CgFileDocument />
-            <Link to="/resume">my resume</Link>
-          </div>
-        </header>
-      </div>
-      <div
-        className={
-          isOpenNavbar ? "navbar-mobile-active navbar-mobile" : "navbar-mobile"
-        }
-      >
-        {/* <ul className={isOpenNavbar ? "navbar navbar-active" : "navbar"}>
-          <li>
-            <Link to="/">
-              <AiOutlineHome />
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <AiOutlineUser />
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects">
-              <AiOutlineFundProjectionScreen />
-              Projects
-            </Link>
-          </li>
-        </ul> */}
-        <Navbar isOpen={isOpenNavbar} onClickLeft={changeNavbarState} />
-      </div>
-    </>
+import "./../assets/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <Link id="logo" to="/">
+        <span>Y.E.</span>
+      </Link>
+      <Navbar />
+    </header>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,30 @@
-import React from "react";
+import { Dispatch, ReactElement, SetStateAction, useState } from "react";
 import { Link } from "react-router-dom";
 import "./../assets/NavBar.css";
-import {
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-const Navbar = ({
-  onClickLeft,
-  isOpen,
-}: {
-  onClickLeft: () => void;
-  isOpen: boolean;
-}) => {
+import { AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
+
+const NavBarLink = ({ Icon, path, cPath, sCPath }: { Icon: ReactElement; path: string; cPath: string; sCPath: Dispatch<SetStateAction<string>> }) => {
   return (
-    <ul className={isOpen ? "navbar-active navbar" : "navbar"}>
-      <li>
-        <Link to="/" onClick={onClickLeft}>
-          <AiOutlineHome />
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" onClick={onClickLeft}>
-          <AiOutlineUser />
-          About
-        </Link>
-      </li>
-      <li>
-        <Link to="/projects" onClick={onClickLeft}>
-          <AiOutlineFundProjectionScreen />
-          Projects
-        </Link>
-      </li>
-    </ul>
+    <li>
+      <Link className={cPath === path ? "active" : ""} onClick={() => sCPath(path)} to={path}>
+        <span>
+          {Icon}
+          hola
+        </span>
+      </Link>
+    </li>
+  );
+};
+const Navbar = () => {
+  const [currentPath, setCurrentPath] = useState("/");
+  return (
+    <nav>
+      <ul className="navbar-ul">
+        <NavBarLink cPath={currentPath} sCPath={setCurrentPath} path="/" Icon={<AiOutlineHome />} />
+        <NavBarLink cPath={currentPath} sCPath={setCurrentPath} path="/projects" Icon={<AiOutlineUser />} />
+        <NavBarLink cPath={currentPath} sCPath={setCurrentPath} path="/contact" Icon={<AiOutlineFundProjectionScreen />} />
+      </ul>
+    </nav>
   );
 };
 
