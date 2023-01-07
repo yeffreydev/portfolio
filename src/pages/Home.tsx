8abf2b32b78f862d88fb22e7f7f05b0baf35ed43
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-06 20:48:57 -0500

    completed home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import svgimg from "./../assets/svg/profile-coder.svg";
 import "./../assets/css/Home.css";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="content-container">
@@ -11,12 +12,20 @@ const Home = () => {
             <img src={svgimg} alt="profile-coder" />
           </div>
           <div className="home-presentation">
-            <h3>Yeffrey Coder</h3>
+            <h3> Yefrey - A Full-Stack Software Developer</h3>
             <p>
-              Lorem, ipsum dolor sit amet consectetur <br /> adipisicing elit. Tenetur distinctio vero provident facere consequuntur repellat officia quia repudiandae impedit <br /> ad, expedita
-              dolore sequi culpa temporibus cumque fugiat tempore dolore <br /> consectetur quisquam modi? Deserunt nemo animi doloribus esse expedita blanditiis? Lorem ipsum dolor sit amet,
-              consectetur adipisicing elit. Sed dicta soluta impedit iste nulla optio, dolor vitae, dolore, exercitationem tempore fugit. Libero, maiores quis. Esse sapiente minus accusantium dolores
-              doloremque.
+              I am a skilled software developer with experience in front-end technologies such as <b>JavaScript</b>, <b>HTML5</b>, and <b>CSS3</b>.
+              <br /> I am proficient in popular JavaScript libraries and frameworks such as <b>React</b> and have experience working with back-end technologies like <b>Node.js</b>, <b>Express</b>, and{" "}
+              <b>MySQL</b>.
+              <br /> I am also familiar with NoSQL databases like <b>MongoDB</b>. <br />
+              In my professional experience, I have worked on a variety of projects and have made open source contributions.
+              <br /> I am confident in my ability to develop and maintain full-stack applications and am always eager to learn and grow as a developer.
+              <br />
+              You can see some of my projects{" "}
+              <b>
+                <Link to={"/projects"}>here</Link>
+              </b>
+              .<br /> I am confident in my ability to develop and maintain full-stack applications and am always eager to learn and grow as a developer.
             </p>
           </div>
         </section>
