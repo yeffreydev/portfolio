5717c989a06ab227be26d452519fd73069b17689
yeffreycode <yeffreyespinoza200@gmail.com>
2023-01-09 23:19:16 -0500

    updated profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="header-container">
       <div className="header">
         <Link id="logo" to="/">
-          <span>Y.E.</span>
+          <img width={55} style={{ borderRadius: "5px" }} src="https://media.discordapp.net/attachments/1062032120979128340/1062065900582342686/ye.png" alt="logo" />
         </Link>
         <Navbar />
       </div>
