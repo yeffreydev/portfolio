8c7cf6a83b9857c543f850b7dbf3b633822cb40a
yeffreycode <yeffreyespinoza200@gmail.com>
2023-04-04 15:28:47 -0500

    added new favicon and change home metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import ContactSection from "@/components/ContactSection";
 import HomeSection from "@/components/HomeSection";
 import ProjectsSection from "@/components/ProjectsSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Yeffrey Espinoza - Home",
+  description: "I' Yeffrey, from Peru. Software Developer(frontend, backend, fullstack, nextjs, next 13, node, express)",
+};
 
 export default function HomePage() {
   return (
