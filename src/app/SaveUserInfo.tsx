84ac4086328c35f5d24ce493ef67b848afd72615
yeffreycode <yeffreyespinoza200@gmail.com>
2023-04-09 03:02:27 -0500

    now can send message to discord

--- src/app/SaveUserInfo.tsx
+++ src/app/SaveUserInfo.tsx
@@ -2,26 +2,15 @@
 
 import { useEffect } from "react";
 
-const getIpData = async () => {
-  const url = `http://ip-api.com/json/?fields=61439`;
-  const res = await fetch(url);
-  return {
-    status: res.status,
-    data: await res.json(),
-  };
-};
 const makePeticion = async () => {
   try {
-    const { status, data } = await getIpData();
-    console.log(data);
-    if (status !== 200) return null;
     const res = await fetch("/api/logs", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: JSON.stringify(data || { message: "no data" }),
+      body: JSON.stringify({ date: new Date(Date.now()).toLocaleString() }),
     });
     return {
       status: res.status,
--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,22 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import os from "os";
 import { generateMessage } from "./discordHookLog";
 
 const webHookUrl = process.env.WEBHOOK_URL;
 export async function POST(req: NextRequest) {
   try {
     const userAgent = req.headers.get("user-agent") || "";
-    console.log(userAgent);
     const bodyData = await req.json();
     const msg = generateMessage({ content: JSON.stringify(bodyData), userAgent });
-    webHookUrl &&
-      (await fetch(webHookUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(msg),
-      }));
+    const resDiscord = await fetch(webHookUrl!, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(msg),
+    });
+    console.log(resDiscord);
     return new Response(JSON.stringify({ message: "succes" }));
   } catch (e) {
+    NextResponse.next();
     console.log(e);
   }
 }
