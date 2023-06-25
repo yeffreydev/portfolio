5a37fce2f4794d6d627bfc1e6e79274853f23d5b
yeffreydev <yeffreyespinoza200@gmail.com>
2023-06-25 03:36:28 -0500

    added two projects in the projects page

--- src/app/projects/ProjectItem.tsx
+++ src/app/projects/ProjectItem.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { BiLinkExternal } from "react-icons/bi";
-function ProjectItem() {
+function ProjectItem({ title, description, deployLink, codeLink, image }: { title: string; description: string; deployLink: string; codeLink: string; image: string }) {
   return (
     <div className="flex flex-col gap-2 max-w-[400px] mt-5 hover:bg-gray-600 hover:bg-opacity-50 p-2.5 rounded-lg">
-      <div className="bg-black h-[200px] w-full"></div>
-      <div className="flex-1">
-        <h2 className="text-2xl font-bold">This is a title</h2>
-        <p className="mt-2 text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quia. Ipsa modi, adipisci atque soluta autem nemo nesciunt ipsam et, fugiat consequatur veniam, a laudantium tenetur! Aut
-          amet nesciunt veniam?
-        </p>
+      <div className="bg-black h-[200px] w-full relative">
+        <img className="w-full h-full absolute object-cover" src={image} alt="" />
+      </div>
+      <div className="flex-1 flex flex-col">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <p className="mt-2 text-justify flex-1">{description}</p>
         <div className="flex gap-3 mt-1">
-          <button className="bg-black text-sm px-3 rounded-lg flex items-center gap-3 hover:text-white">
-            <span>Code</span>
-            <FaGithub />
-          </button>
-          <button className="bg-black text-sm px-3 rounded-lg flex items-center gap-3 hover:text-white">
+          {codeLink && (
+            <a href={codeLink} target="_blank" rel="noreferrer" className="bg-black text-sm px-3 rounded-lg flex items-center gap-3 hover:text-white">
+              <span>Code</span>
+              <FaGithub />
+            </a>
+          )}
+          <a href={deployLink} target="_blank" rel="noreferrer" className="bg-black text-sm px-3 rounded-lg flex items-center gap-3 hover:text-white">
             <span>deploy</span>
             <BiLinkExternal />
-          </button>
+          </a>
         </div>
       </div>
     </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+const projects = [
+  {
+    title: "Pomodoro and Timer App",
+    description: `this project was developed using only HTML, css and Javascript and it's a simple app with basic pomodoro and timer functionality that I built to practice my skills.`,
+    deployLink: "https://yeffreydev.github.io/timer-and-pomo-app/",
+    codeLink: "https://github.com/yeffreydev/timer-and-pomo-app",
+    image: "/projects/timer-and-pomo.png",
+  },
+  {
+    title: "Soccer App",
+    description: ` I developed this app for practice my backend skills,  data structure and real time connection, this project was developed with NextJs for frontend, and NodeJs with Typescript for backend. I used Socket.io for real time connection and MongoDB for database.`,
+    deployLink: "https://bb200-client.vercel.app/app",
+    codeLink: "",
+    image: "/projects/soccer-app.png",
+  },
+];
+
 function page() {
   return (
     <div className="mt-[70px] w-11/12 mx-auto flex flex-wrap gap-3 justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+        {projects.map((project, index) => {
+          return <ProjectItem image={project.image} codeLink={project.codeLink} deployLink={project.deployLink} key={index} description={project.description} title={project.title} />;
+        })}
       </div>
     </div>
   );
