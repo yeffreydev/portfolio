4c471b92e8ea92f4b3e20acdff4203372bb34241
yeffreydev <yeffreyespinoza200@gmail.com>
2023-06-25 19:24:04 -0500

    added 1 project and other little changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,17 +4,26 @@ import ProjectItem from "./ProjectItem";
 const projects = [
   {
     title: "Pomodoro and Timer App",
-    description: `this project was developed using only HTML, css and Javascript and it's a simple app with basic pomodoro and timer functionality that I built to practice my skills.`,
+    description: `This project was developed using only HTML, CSS, and JavaScript. It is a simple app with basic pomodoro and timer functionality that I built to practice my skills.`,
     deployLink: "https://yeffreydev.github.io/timer-and-pomo-app/",
     codeLink: "https://github.com/yeffreydev/timer-and-pomo-app",
     image: "/projects/timer-and-pomo.png",
   },
   {
     title: "Soccer App",
-    description: ` I developed this app for practice my backend skills,  data structure and real time connection, this project was developed with NextJs for frontend, and NodeJs with Typescript for backend. I used Socket.io for real time connection and MongoDB for database.`,
+    description: ` I developed this app to practice my backend skills, data structure, and real-time connections. This project was developed with NextJs for the frontend and NodeJs with Typescript for the backend. I used Socket.io for real-time connections and MongoDB for the database.`,
     deployLink: "https://bb200-client.vercel.app/app",
     codeLink: "",
     image: "/projects/soccer-app.png",
+  },
+  {
+    title: "Gest Fina",
+    description: `
+    University Project: A fundamental Android app, developed with React Native, for managing finances. It allows transaction editing and real-time balance monitoring.
+    `,
+    deployLink: "https://gest-fina.netlify.app/",
+    codeLink: "https://github.com/yeffreydev/gestFina-innovationProject",
+    image: "/projects/gest-fina-app.png",
   },
 ];
 
