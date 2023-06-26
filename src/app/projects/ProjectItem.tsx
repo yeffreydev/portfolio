44a5f87db9bacb64e2db239fcf7ca97f7fe8d6d4
yeffreydev <yeffreyespinoza200@gmail.com>
2023-06-25 23:04:29 -0500

    added link to image in project item

--- src/app/projects/ProjectItem.tsx
+++ src/app/projects/ProjectItem.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { BiLinkExternal } from "react-icons/bi";
+import styles from "./ProjectItem.module.css";
 function ProjectItem({ title, description, deployLink, codeLink, image }: { title: string; description: string; deployLink: string; codeLink: string; image: string }) {
   return (
     <div className="flex flex-col gap-2 max-w-[400px] mt-5 hover:bg-gray-600 hover:bg-opacity-50 p-2.5 rounded-lg">
       <div className="bg-black h-[200px] w-full relative">
-        <img className="w-full h-full absolute object-cover" src={image} alt="" />
+        <a href={deployLink} className={`block ${styles["img-link"]} w-full h-full`} rel="noreferrer" target="_blank">
+          <img className="w-full h-full absolute object-cover" src={image} alt="" />
+          <BiLinkExternal className="opacity-0 top-1 right-1 text-white  absolute" />
+        </a>
       </div>
       <div className="flex-1 flex flex-col">
         <h2 className="text-2xl font-bold">{title}</h2>
---
+++ src/app/projects/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata = {
+  title: "Projects - Yeffrey Espinoza",
+  description: "list of my projects.",
+  keywords: "Yeffrey Espinoza, yeffrey espinoza, yeffrey espinoza projects,freelancer, reactjs, nodejs freelancer, yeffrey.dev",
+};
+export default function ProjectLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
