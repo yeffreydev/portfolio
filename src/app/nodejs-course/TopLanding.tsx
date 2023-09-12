382f0df000c9f8a0a966cf2817b4d72ed88af200
yeffreydev <yeffreyespinoza200@gmail.com>
2023-09-11 23:12:28 -0500

    agregado el link del meet de la presentación de la presentación del curso y tambien se agregó el grupo de whatsapp

--- src/app/nodejs-course/TopLanding.tsx
+++ src/app/nodejs-course/TopLanding.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
+import { FiExternalLink } from "react-icons/fi";
 export default function TopLanding() {
   return (
     <div className="flex flex-col items-start w-11/12 mx-auto mt-20 gap-5">
@@ -8,14 +9,22 @@ export default function TopLanding() {
           <h1 className="text-3xl font-semibold">Curso de NodeJS - version 1.0</h1>
           <p>
             ¡Bienvenidos al Curso de NodeJS - Versión 1.0! Mi nombre es Yeffrey Espinoza y soy de Perú. Hoy, me complace compartir con ustedes mi experiencia y conocimientos en el mundo de la
-            programación, especialmente en NodeJS.
+            programación, especialmente en NodeJS. <br />
+            puedes ver la presentacion del curso{" "}
+            <a className="underline" href="https://drive.google.com/file/d/1ol1aouZGq6ATRJqC49KoUQ0lNg8f7en1/view?usp=sharing" target="_blank">
+              Aquí
+              <FiExternalLink className="inline-block" />
+            </a>
           </p>
         </div>
         <div className="flex-1 grid md:place-items-center">
           <Image src={"https://th.bing.com/th/id/OIP.hv_EzUOseMZeJiHvRn9LEwHaEh?pid=ImgDet&rs=1"} width={300} height={200} alt="nodejs img" />
         </div>
       </div>
-      <Link href="/nodejs-course/login" className="bg-black rounded-3xl text-white px-7 py-2">Ingresar al Curso</Link>
+      <Link href="/nodejs-course/login" className="bg-black rounded-3xl text-white px-7 py-2">
+        Unirme Al Curso
+        {/* si esta autenticado mostrar ver curso  */}
+      </Link>
     </div>
   );
 }
--- src/app/nodejs-course/login/page.tsx
+++ src/app/nodejs-course/login/page.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const LoginCoursePage = () => {
-    return (
-        <div className="border mt-10 md:mt-20">
-            <form className="mx-auto w-11/12 shadow-xl md:w-4/12 flex flex-col gap-5 p-10 rounded">
-                <div className="w-full">
-                    <input className="w-full bg-transparent border-b border-black" placeholder="User Id" />
-                </div>
-                <div className="w-full">
-                    <input type="password" className="w-full bg-transparent border-b border-black" placeholder="Password" />
-                </div>
-                <div className="w-full flex">
-                    <input className="bg-black mx-auto w-8/12 md:w-6/12 text-white font-bold cursor-pointer rounded-3xl py-2" type="submit" value="Login" />
-                </div>
-            </form>
+  return (
+    <div className="border mt-10 md:mt-20">
+      <form className="mx-auto w-11/12 shadow-xl md:w-4/12 flex flex-col gap-5 p-10 rounded">
+        <div className="w-full">
+          <input className="w-full bg-transparent border-b border-black" placeholder="User Id" />
         </div>
-    )
-}
+        <div className="w-full">
+          <input type="password" className="w-full bg-transparent border-b border-black" placeholder="Password" />
+        </div>
+        <div className="w-full flex">
+          <input className="bg-black mx-auto w-8/12 md:w-6/12 text-white font-bold cursor-pointer rounded-3xl py-2" type="submit" value="Login" />
+        </div>
+        <div>
+          <p>Si aun no estás dentro del curso</p>
+          <p>Puedes unirte al grupo y enviar un mensaje al administrador</p>
+          <a className="underline" href="https://chat.whatsapp.com/KN1ROVEzjLiCkn7nmicJM4" target="_blank">
+            Unirme Al Grupo
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default LoginCoursePage
+export default LoginCoursePage;
