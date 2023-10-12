1cb3da04ec9ee4ebf36ac9bcdaf339f94f11c7d0
yeffreydev <yeffreyespinoza200@gmail.com>
2023-10-12 12:03:10 -0500

    translate loading text

--- src/app/nodejs-course/content/loading.tsx
+++ src/app/nodejs-course/content/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="mt[100px]">
-      <span className="text-xl font-bold text-center mx-auto">Cargando ...</span>
+      <span className="text-xl font-bold text-center mx-auto">Cargando...</span>
     </div>
   );
 }
