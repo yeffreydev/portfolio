0153fd7270f53ac9a3a679888027647a70513d1b
yeffreydev <yeffreyespinoza200@gmail.com>
2024-02-04 05:30:29 -0500

    added main mentoring services

--- src/app/services/mentoring/page.tsx
+++ src/app/services/mentoring/page.tsx
@@ -3,35 +3,49 @@ import MentoringCard from "./MentoringCard";
 const content = [
   {
     title: "Mentor Personal",
-    price: "$100 / mes",
+    price: "$50 / mes",
+    description: `
+      Ideal si has estado luchando por alcanzar tus objetivos en este campo y aún no lo has logrado. No significa que no tengas la capacidad innata para ello; más bien, son los miedos y la falta de disciplina los que te están limitando. En esta mentoría mensual, me comprometo a ayudarte a superar esos miedos, a cultivar la constancia y la disciplina en tu programación, guiándote hasta que alcances la meta de convertirte en un desarrollador exitoso capaz de crear productos y servicios impactantes para el mundo.`,
+    link: "#",
+  },
+  {
+    title: "Comunidad Premium.",
+    price: "$10 / mes",
+    description:
+      "Todos los miembros de la comunidad premium tienen la oportunidad de participar de retos de programación y ganar premios, colaborar en proyectos, además de tener charlas de más de una hora por Meet o Discord. Durante estas sesiones, podrán compartir y escuchar experiencias como desarrolladores. Ver mas informacionn, (el dinero de esta membresia es para dar premios y financiar algunos eventos de la comunidad)",
+    link: "#",
+  },
+  {
+    title: "Asesoria Vence Tus Miedos y gana oportunidades.",
+    price: "$30, 1 hora + 1 mes comunidad premium.",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium error optio maiores consectetur vitae voluptatibus, fugit magni iure suscipit corporis a beatae sint ex
     quibusdam? Expedita voluptates modi dolorum?`,
     link: "#",
   },
   {
-    title: "Mentor Personal",
-    price: "$100 / mes",
+    title: "Asesoria Monetiza tus habilidades.",
+    price: "$30 / 1 hora.+ 1 mes comunida premium",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium error optio maiores consectetur vitae voluptatibus, fugit magni iure suscipit corporis a beatae sint ex
     quibusdam? Expedita voluptates modi dolorum?`,
     link: "#",
   },
   {
-    title: "Mentor Personal",
-    price: "$100 / mes",
+    title: "Desarrolla conmigo un proyecto Full Stack.",
+    price: "$30",
+    description:
+      "En este curso aprenderas a crear un proyecto con Next, Tailwind, MongoDB, NodeJS, Auth de usuarios con google, facebook y email, chat en tiempo real, subir archivos, estilos cont tailwind. tipo red social. sistema de likes y comentarios. ",
+    link: "#",
+  },
+  {
+    title: "Comunidad de whatsapp",
+    price: "$Free",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium error optio maiores consectetur vitae voluptatibus, fugit magni iure suscipit corporis a beatae sint ex
     quibusdam? Expedita voluptates modi dolorum?`,
     link: "#",
   },
   {
-    title: "Mentor Personal",
-    price: "$100 / mes",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium error optio maiores consectetur vitae voluptatibus, fugit magni iure suscipit corporis a beatae sint ex
-    quibusdam? Expedita voluptates modi dolorum?`,
-    link: "#",
-  },
-  {
-    title: "Mentor Personal",
-    price: "$100 / mes",
+    title: "Comunidad de discord",
+    price: "$Free",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium error optio maiores consectetur vitae voluptatibus, fugit magni iure suscipit corporis a beatae sint ex
     quibusdam? Expedita voluptates modi dolorum?`,
     link: "#",
